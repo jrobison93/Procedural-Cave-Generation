54f691316b0652734ae3ea2d8640a355a614e1d5
John Robison <10904512@live.mercer.edu>
2016-03-13 03:48:05 -0400

    Refactored the Node classes out of the MeshGenerator.

--- Unity Project/Assets/Scripts/MeshGenerator.cs
+++ Unity Project/Assets/Scripts/MeshGenerator.cs
@@ -364,26 +364,4 @@ public class MeshGenerator : MonoBehaviour {
 		}
 
 	}
-
-	public class Node {
-		public Vector3 position;
-		public int vertexIndex = -1;
-
-		public Node(Vector3 _pos) {
-			position = _pos;
-		}
-	}
-
-	public class ControlNode : Node {
-
-		public bool active;
-		public Node above, right;
-
-		public ControlNode(Vector3 _pos, bool _active, float squareSize) : base(_pos) {
-			active = _active;
-			above = new Node(position + Vector3.forward * squareSize/2f);
-			right = new Node(position + Vector3.right * squareSize/2f);
-		}
-
-	}
 }
