62a8a99f46050ce71df256fb2f0ea188b5d82cf8
John Robison <10904512@live.mercer.edu>
2016-03-12 19:36:28 -0500

    Added a Unit Testing Utility class.

---
+++ Unity Project/Assets/Editor/UnitTestUtilities.cs
@@ -0,0 +1,51 @@
+﻿/// This Utility Class was derived from examples found at:
+/// Code Project (http://www.codeproject.com/Articles/9715/How-to-Test-Private-and-Protected-methods-in-NET)
+/// and Stack Overflow (http://stackoverflow.com/questions/3303126/how-to-get-the-value-of-private-field-in-c)
+
+using System;
+using System.Reflection;
+
+namespace Assets.Editor
+{
+    class UnitTestUtilities
+    {
+        public static object RunStaticMethod(Type t, string strMethod, object[] aobjParams)
+        {
+            BindingFlags eFlags = BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic;
+            return RunMethod(t, strMethod, null, aobjParams, eFlags);
+        }
+
+        public static object RunInstanceMethod(Type t, string strMethod, object objInstance, object[] aobjParams)
+        {
+            BindingFlags eFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic;
+            return RunMethod(t, strMethod, objInstance, aobjParams, eFlags);
+        }
+
+        private static object RunMethod(Type t, string strMethod, object objInstance, object[] aobjParams, BindingFlags eFlags)
+        {
+            MethodInfo m;
+            try
+            {
+                m = t.GetMethod(strMethod, eFlags);
+                if (m == null)
+                {
+                    throw new ArgumentException("There is no method '" + strMethod + "' for type '" + t.ToString() + "'.");
+                }
+
+                object objRet = m.Invoke(objInstance, aobjParams);
+                return objRet;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public static object GetInstanceField(Type type, object instance, string fieldName)
+        {
+            BindingFlags bindFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static;
+            FieldInfo field = type.GetField(fieldName, bindFlags);
+            return field.GetValue(instance);
+        }
+    }
+}
